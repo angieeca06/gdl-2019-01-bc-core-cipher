1835be73c5afb24ad36edf8ee1a60b60948253b7
unknown <angyalva16@gmail.com>
2019-02-05 11:55:11 -0600

    Colocar estilos en css

--- src/cipher.js
+++ src/cipher.js
@@ -2,20 +2,30 @@ window.cipher = {
 
    encode: (offSet, letter) =>{
     let result = '';
+    letter=letter.toUpperCase();
     for(let i=0; i<letter.length; i++){
-        let nletra = letter.charCodeAt(i);
-        let formula = ((nletra - 65 + offSet) % 26 )+ 65;
-        result = result + (String.fromCharCode(formula));
+        if(letter.charCodeAt(i) === 32){
+            result = result + (String.fromCharCode(32));
+        }else{
+            let nletra = letter.charCodeAt(i);
+            let formula = ((nletra - 65 + offSet) % 26 )+ 65;
+            result = result + (String.fromCharCode(formula));
+        }
     }
     return result;
 },
 
   decode: (offSet,letter) =>{
    let result = "";
+   letter=letter.toUpperCase();
     for(let i=0; i<letter.length; i++){
-        let nletra = letter.charCodeAt(i);
-        let formula = ((nletra + 65 - offSet) % 26 )+ 65;
-        result = result + (String.fromCharCode(formula));
+        if(letter.charCodeAt(i) === 32){
+            result = result + (String.fromCharCode(32));
+        }else{
+            let nletra = letter.charCodeAt(i);
+            let formula = ((nletra + 65 - offSet) % 26 )+ 65;
+            result = result + (String.fromCharCode(formula));
+        }
     }
    return result;
   
--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const datesEncode = () =>{
 const datesDecode = () =>{
     const offSet = parseInt(document.getElementById('posicionesD').value);
     const letter = document.getElementById('mensajeD').value;
-    const result = window.cipher.decode(letter, offSet);
+    const result = window.cipher.decode(offSet, letter);
     document.getElementById('resultadoD').innerHTML = result;
-}
+};
 document.getElementById('enviarP4').addEventListener('click',datesDecode);
 
 const selectCipher = () =>{
@@ -19,7 +19,7 @@ const selectCipher = () =>{
     const show = document.getElementById('Page2');
     hide.style.display = 'none';
     show.style.display = 'block';
-}
+};
 document.getElementById('cifrar').addEventListener('click',selectCipher);
 
 const selectDescipher = () =>{
@@ -27,23 +27,25 @@ const selectDescipher = () =>{
     const show = document.getElementById('Page4');
     hide.style.display = 'none';
     show.style.display = 'block';
-}
+};
 document.getElementById('descifrar').addEventListener('click',selectDescipher);
 
-const menssageCipher = () =>{
+const menssageCipher = (event) =>{
+    event.preventDefault();
     const hide = document.getElementById('Page2');
     const show = document.getElementById('Page3');
     hide.style.display = 'none';
     show.style.display = 'block';
-}
+};
 document.getElementById('enviarP3').addEventListener('click', menssageCipher);
 
-const menssageDescipher = () =>{
+const menssageDescipher = (event) =>{
+    event.preventDefault();
     const hide = document.getElementById('Page4');
     const show = document.getElementById('Page5');
     hide.style.display = 'none';
     show.style.display = 'block';
-}
+};
 document.getElementById('enviarP4').addEventListener('click',menssageDescipher);
 
 const pageStarD = () =>{
@@ -51,7 +53,7 @@ const pageStarD = () =>{
     const show = document.getElementById('Page1');
     hide.style.display = 'none';
     show.style.display = 'block';
-}
+};
 document.getElementById('enviarP1D').addEventListener('click',pageStarD);
 
 const pageStarC = () =>{
@@ -59,5 +61,5 @@ const pageStarC = () =>{
     const show = document.getElementById('Page1');
     hide.style.display = 'none';
     show.style.display = 'block';
-}
+};
 document.getElementById('enviarP1C').addEventListener('click',pageStarC);
