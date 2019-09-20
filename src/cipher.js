64e9f0cf4edfd951c491b6cba9bb8488f4f5d0c1
angie <angyalva16@gmail.com>
2019-09-20 12:50:21 -0500

    Clean principal functions

--- src/cipher.js
+++ src/cipher.js
@@ -2,32 +2,39 @@ window.cipher = {
 
    encode: (offSet, letter) =>{
     let result = '';
-    letter=letter.toUpperCase();
     for(let i=0; i<letter.length; i++){
-        if(letter.charCodeAt(i) === 32){
-            result = result + (String.fromCharCode(32));
-        }else{
+        let nAscii = letter.charCodeAt(i)
+        if(nAscii>=97 && nAscii<=122){
+            let nletra = letter.charCodeAt(i);
+            let formula = ((nletra - 97 + offSet) % 26 )+ 97;
+            result = result + (String.fromCharCode(formula));
+        }else if(nAscii >= 65 && nAscii <= 90 ){
             let nletra = letter.charCodeAt(i);
             let formula = ((nletra - 65 + offSet) % 26 )+ 65;
             result = result + (String.fromCharCode(formula));
+        }else{
+            result = result + (String.fromCharCode(nAscii));
         }
     }
     return result;
 },
 
   decode: (offSet,letter) =>{
-   let result = "";
-   letter=letter.toUpperCase();
+    let result = '';
     for(let i=0; i<letter.length; i++){
-        if(letter.charCodeAt(i) === 32){
-            result = result + (String.fromCharCode(32));
-        }else{
+        let nAscii = letter.charCodeAt(i)
+        if(nAscii>=97 && nAscii<=122){
+            let nletra = letter.charCodeAt(i);
+            let formula = ((nletra + 97 - offSet) % 26 )+ 97;
+            result = result + (String.fromCharCode(formula));
+        }else if(nAscii >= 65 && nAscii <= 90 ){
             let nletra = letter.charCodeAt(i);
             let formula = ((nletra + 65 - offSet) % 26 )+ 65;
             result = result + (String.fromCharCode(formula));
+        }else{
+            result = result + (String.fromCharCode(nAscii));
         }
     }
-   return result;
-  
+    return result;
 }
 };
